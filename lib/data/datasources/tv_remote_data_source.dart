d2e63dcd8212205d069829b84bbd920f0f801ed2
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2023-09-19 23:16:26 +0700

    Add remote data sources

---
+++ lib/data/datasources/tv_remote_data_source.dart
@@ -0,0 +1,101 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:movie_tv/common/constants.dart';
+import 'package:movie_tv/common/exception.dart';
+import 'package:movie_tv/data/models/tv_detail_model.dart';
+import 'package:movie_tv/data/models/tv_model.dart';
+import 'package:movie_tv/data/models/tv_response.dart';
+
+abstract class TvShowRemoteDataSource {
+  Future<List<TvModel>> getOnTheAirTvShows();
+
+  Future<List<TvModel>> getPopularTvShows();
+
+  Future<List<TvModel>> getTopRatedTvShows();
+
+  Future<TvDetailResponse> getTvShowDetail(int id);
+
+  Future<List<TvModel>> getTvShowRecommendations(int id);
+
+  Future<List<TvModel>> searchTvShows(String query);
+}
+
+class TvShowRemoteDataSourceImpl implements TvShowRemoteDataSource {
+  final http.Client client;
+
+  TvShowRemoteDataSourceImpl({required this.client});
+
+  static const tv = 'tv';
+
+  @override
+  Future<List<TvModel>> getOnTheAirTvShows() async {
+    final response =
+        await client.get(Uri.parse('$baseUrl/$tv/on_the_air?$apiKey'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).tvList;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<TvModel>> getPopularTvShows() async {
+    final response =
+        await client.get(Uri.parse('$baseUrl/$tv/popular?$apiKey'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).tvList;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<TvModel>> getTopRatedTvShows() async {
+    final response =
+        await client.get(Uri.parse('$baseUrl/$tv/top_rated?$apiKey'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).tvList;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<TvDetailResponse> getTvShowDetail(int id) async {
+    final response = await client.get(Uri.parse('$baseUrl/$tv/$id?$apiKey'));
+
+    if (response.statusCode == 200) {
+      return TvDetailResponse.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<TvModel>> getTvShowRecommendations(int id) async {
+    final response =
+        await client.get(Uri.parse('$baseUrl/$tv/$id/recommendations?$apiKey'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).tvList;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<TvModel>> searchTvShows(String query) async {
+    final response =
+        await client.get(Uri.parse('$baseUrl/search/$tv?$apiKey&query=$query'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).tvList;
+    } else {
+      throw ServerException();
+    }
+  }
+}
