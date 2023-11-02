e3eeb228993df6ee15b9116fe7d78a3115b33608
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2023-11-02 13:04:56 +0700

    Fix path file

--- packages/core/test/json_reader.dart
+++ packages/core/test/json_reader.dart
@@ -2,8 +2,5 @@ import 'dart:io';
 
 String readJson(String directory, String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('tmdb')) {
-    dir = dir.replaceAll('tmdb', 'tmdb/packages');
-  }
-  return File('$dir/$directory/test/$name').readAsStringSync();
+  return File('$dir/packages/$directory/test/$name').readAsStringSync();
 }
